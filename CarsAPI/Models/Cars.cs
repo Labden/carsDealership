426b26dee16f8bab46ef0bc4ed9a43259d3e625b
Labden <nedbal.trent@gmail.com>
2021-07-07 09:15:20 -0400

    carsDealership

---
+++ CarsAPI/Models/Cars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CarsAPI.Models
+{
+    public partial class Cars
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int? Year { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ CarsAPI/Models/CarsDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CarsAPI.Models
+{
+    public partial class CarsDBContext : DbContext
+    {
+        public CarsDBContext()
+        {
+        }
+
+        public CarsDBContext(DbContextOptions<CarsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cars> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CarsDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cars>(entity =>
+            {
+                entity.Property(e => e.Color)
+                    .HasColumnName("color")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Make)
+                    .HasColumnName("make")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Model)
+                    .HasColumnName("model")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Year).HasColumnName("year");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
